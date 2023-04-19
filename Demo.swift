68f1ba08764e36c24dcca294b2cd1c9372f08a41
Shyam Changela <shyam@dxfactor.com>
2023-04-19 14:23:43 +0530

    first commit

---
+++ Demo.swift
@@ -0,0 +1,8 @@
+//
+//  Demo.swift
+//  SCManager
+//
+//  Created by Shyam Changela on 19/04/23.
+//
+
+import Foundation
---
+++ OSLogger.swift
@@ -0,0 +1,8 @@
+//
+//  OSLogger.swift
+//  SCManager
+//
+//  Created by Shyam Changela on 19/04/23.
+//
+
+import Foundation
